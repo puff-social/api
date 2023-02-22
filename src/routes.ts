a92f3f97092a136c9fd4dce43596e15fad3b9f1d
Dustin Rouillard <code@dstn.to>
2023-02-22 00:58:37 -0700

    Update for tracking

--- src/routes.ts
+++ src/routes.ts
@@ -2,19 +2,48 @@ import { FastifyInstance, FastifyPluginOptions } from "fastify";
 import { ZodError } from "zod"
   ;
 import { pika } from "./pika";
-import { feedbackValidation } from "./utils";
+import { feedbackValidation, trackingValidation } from "./utils";
 import { prisma } from "./connectivity/prsima";
 
 export function Routes(server: FastifyInstance, opts: FastifyPluginOptions, next: () => void) {
-  server.post('/track', (req, res) => {
-    console.log(req.body);
+  server.get('/leaderboard', async (req, res) => {
+    const leaderboards = await prisma.leaderboard.findMany({ orderBy: { total_dabs: 'desc' }, take: 10 });
+
+    return res.status(200).send({ success: true, data: { leaderboards } });
+  });
+
+  server.post('/track', async (req, res) => {
+    const validate = await trackingValidation.parseAsync(req.body);
+
+    const id = pika.gen('leaderboard');
+    const deviceBirthday = new Date(Number(Buffer.from(validate.device.id, 'base64').toString('utf8')) * 1000);
+
+    const existing = await prisma.leaderboard.findFirst({ where: { device_id: validate.device.id } });
+    if (existing) {
+      await prisma.leaderboard.update({
+        data: {
+          device_name: validate.device.name, device_birthday: deviceBirthday, owner_name: validate.name, total_dabs: validate.device.totalDabs
+        },
+        where: {
+          device_id: validate.device.id
+        }
+      });
+    } else {
+      await prisma.leaderboard.create({
+        data: {
+          id, device_id: validate.device.id, device_name: validate.device.name, device_birthday: deviceBirthday, owner_name: validate.name, total_dabs: validate.device.totalDabs
+        }
+      });
+    }
+
     return res.status(204).send();
   });
 
   server.post('/feedback', async (req, res) => {
     try {
-      const id = pika.gen('feedback');
       const validate = await feedbackValidation.parseAsync(req.body);
+
+      const id = pika.gen('feedback');
       const ip = (req.headers['cf-connecting-ip'] || req.connection.remoteAddress || '0.0.0.0') as string;
 
       await prisma.feedback.create({
--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,12 @@ import { z } from "zod";
 export const feedbackValidation = z.object({
   message: z.string().max(1024)
 });
+
+export const trackingValidation = z.object({
+  device: z.object({
+    id: z.string(),
+    name: z.string(),
+    totalDabs: z.number(),
+  }),
+  name: z.string()
+});
