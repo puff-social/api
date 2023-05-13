77e6893a0885d9460bd495a6ee0d3237038f0530
Dustin Rouillard <code@dstn.to>
2023-05-12 21:53:45 -0600

    Adding avg_position for avg_dabs

--- prisma/functions.sql
+++ prisma/functions.sql
@@ -4,19 +4,21 @@ CREATE OR REPLACE FUNCTION public.recalc_leaderboard()
  LANGUAGE plpgsql
 AS $function$
 BEGIN
-  INSERT INTO device_leaderboard (id, position)
+  INSERT INTO device_leaderboard (id, position, avg_position)
 SELECT
   id,
-  RANK() OVER (ORDER BY dabs DESC)
+  RANK() OVER (ORDER BY dabs DESC) as rank,
+  RANK() OVER (ORDER BY avg_dabs DESC) as avg_rank
 FROM
-  devices ON CONFLICT (id)
+  devices WHERE user_id IS NOT NULL ON CONFLICT (id)
   DO
   UPDATE
   SET
-    position = EXCLUDED.position;
+    position = EXCLUDED.position,
+    avg_position = EXCLUDED.avg_position;
   RETURN NEW;
 END
-$function$
+$function$;
 
 -- Call the recalc_position() function when leaderboard UPDATEs --
 CREATE TRIGGER device_recalc_on_update
